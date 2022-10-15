2f939c18c028c33083867d7f38effc245e84ae33
Musabii Mambetov <musabi@live.com>
2022-10-16 01:34:43 +0300

    HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, HashRouter, Navigate, Route, Routes } from 'react-router-dom'
 import App from './App'
 import './style.scss'
 import { Auth } from './pages/auth'
 import { Chat } from './pages/chat'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
-      <Route path='/' element={<Navigate to="/auth" replace/>} />
+      <Route path='/' element={<Navigate to="/auth" replace />} />
       <Route path='/auth' element={<Auth />} />
       <Route path='/chat' element={<Chat />} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 )
